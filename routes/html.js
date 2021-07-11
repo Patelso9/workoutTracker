f6a2770909ba6f34e80d070733d35237564b95c4
Princess <sonali.patel.pkg@gmail.com>
2021-07-10 22:05:36 -0500

    dirname actually fixed, miss type

--- routes/html.js
+++ routes/html.js
@@ -4,20 +4,20 @@ const path = require("path");
 //localhost:300/
 router.get('/', (req, res) => {
     console.log ('html GET /')
-    res.sendFile(path.join(_dirname, '../public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
 
 //localhost:300/excercise
 router.get('/excercise', (req, res) => {
     console.log ('html GET /exercise')
-    res.sendFile(path.join(_dirname, '../public/exercise.html'))
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
 });
 
 //localhost:300/stats
 router.get('/stats', (req, res) => {
     console.log ('html GET /stats')
-    res.sendFile(path.join(_dirname, '../public/stats.html'))
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
 });
 
 module.exports = router;
